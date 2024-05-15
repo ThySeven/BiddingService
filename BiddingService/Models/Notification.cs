8ff998634205dbdf449e99b682d187a3b0d4f5cd
Wufferman <eaajbst@students.eaaa.dk>
2024-05-15 13:21:54 +0200

    notificationModel fix

--- BiddingService/Models/Notification.cs
+++ BiddingService/Models/Notification.cs
@@ -6,7 +6,7 @@
         public string RecieverMail { get; set; }
         public DateTime TimeStamp { get; set; }
         public string LotName { get; set; }
-        public string NewLotPrice { get; set; }
+        public int NewLotPrice { get; set; }
         public string NewBidLink { get; set; }
 
     }
--- BiddingService/obj/Debug/net8.0/BiddingService.AssemblyInfo.cs
+++ BiddingService/obj/Debug/net8.0/BiddingService.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BiddingService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7031666063ee0c44d3262e67a69df28732a680b8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a068e06bd1b9307b783f16e6811c719242e130f")]
 [assembly: System.Reflection.AssemblyProductAttribute("BiddingService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BiddingService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
