f7d9dfeedd29ccc1545b7eda0c050443d03a9355
Augustmunkhaus <fordemkidz69@gmail.com>
2024-05-21 10:50:15 +0200

    byd igen added

---
+++ BiddingServiceTest/UnitTest1.cs
@@ -0,0 +1,32 @@
+using BiddingService.Controllers;
+using BiddingService.Models;
+using BiddingService.Repository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using MongoDB.Driver;
+using Moq;
+using NUnit.Framework;
+using RabbitMQ.Client;
+using System;
+using System.Threading.Tasks;
+
+namespace BiddingServiceTest
+{
+    public class Tests
+    {
+        
+
+        // public void MockDeezNutz();
+
+
+
+        
+
+
+
+
+
+
+    } 
+}
